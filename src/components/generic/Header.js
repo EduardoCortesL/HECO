8ddc2fe5ec2547596bdf9151e48addd84f689e0d
Eduardo CL <corteslalo97@gmail.com>
2023-07-05 22:04:32 -0700

    added some images

--- src/components/generic/Header.js
+++ src/components/generic/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 	return (
 		<div className="bg-transparent">
 			<div className="m-auto max-w-6xl flex items-center justify-between">
-					<Image src="/HECO_logo_no_bg.png" width={300} height={300} alt="logo HECO"/>		
+					<Image src="/HECO_logo_no_bg.png" width={200} height={120} alt="logo HECO"/>		
 			</div>
 		</div>
 	);
--- src/components/landing/HowItWorks.js
+++ src/components/landing/HowItWorks.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
+import Image from "next/image";
 
 const STEPS = [
 	{
 		title: "Cotizamos",
-		subtitle: "Te cotizamos el mejor precio solo para ti"
+		subtitle: "Te cotizamos el mejor precio solo para ti",
+		image: "/01_HECO.jpg"
 	},
 	{
 		title: "Instalamos",
-		subtitle: "En timepo record lo instalamos"
+		subtitle: "En timepo record lo instalamos",
+		image: "/01_HECO.jpg"
 	},
 	{
 		title: "Manejas",
-		subtitle: "El mundo deja de tener limites"
+		subtitle: "El mundo deja de tener limites",
+		image: "/01_HECO.jpg"
 	},
 ];
 
 const Step = ({ step }) => {
 	return (
 		<div className="flex-1 py-4 px-4">
-			<div className="w-full h-64 bg-midnight-blue rounded-lg"></div>
+			<div className="w-full h-64 bg-midnight-blue rounded-lg">
+				<Image src={step.image} width={350} height={260}/>
+			</div>
 			<div className="mt-4">
 				<p className="text-2xl text-gray-600 font-bold mb-2 text-center md:text-left">
 					{step.title}
@@ -33,7 +39,7 @@ const Step = ({ step }) => {
 	);
 }
 
-const HowItWorks = ({ title, subtitle }) => {
+const HowItWorks = ({ title, subtitle, image }) => {
 	return (
 		<div>
 			<div className="m-auto max-w-2xl">
@@ -42,6 +48,9 @@ const HowItWorks = ({ title, subtitle }) => {
 				</h2>
 				<p className="text-center text-2xl font-medium my-8 text-gray-600">
 					{subtitle}
+				</p>
+				<p className="text-center text-2xl font-medium my-8 text-gray-600">
+					{image}
 				</p>
 			</div>
 			<div className="md:flex justify-around m-auto max-w-6xl py-8 md:py-16">
